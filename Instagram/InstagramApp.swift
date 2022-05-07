ac4904a42609f8b77092ec24cb8f24c13048b4b5
Wagão - ( Wagner Goulart ) <wcfx.dev@gmail.com>
2022-05-07 00:48:29 -0300

    chore: default structure

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by WAGNER GOULART on 07/05/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
